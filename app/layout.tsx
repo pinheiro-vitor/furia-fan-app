e5c0d7c3dc5aa15aff40db73a12c85c1484b60b0
Vitor Pinheiro <vitor.pinheiro97@hotmail.com>
2025-04-30 10:40:26 -0300

    att layout: vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -17,7 +18,10 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/furia-icon.ico" type="image/x-icon" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
